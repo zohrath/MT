6d4865ac0018fef9c9b261823d70c22c942c296d
Adam Woods <adam.woods@valtech.com>
2023-10-09 16:28:38 +0200

    Moved stat files around

--- get_random_search_vals_for_opt_ann.py
+++ get_random_search_vals_for_opt_ann.py
@@ -30,22 +30,22 @@ count = 100
 # gwn_std_dev = [0.06427594732038355, 0.09711193898395054, 0.18030623089238926, 0.18926183363254914, 0.17275962871453357,
 #                0.026820486300555134, 0.08580276947147242, 0.19679651652191496, 0.07150807341695263, 0.1315206414296331]
 
-# Cp_min = [0.9, 2.0]
-# Cp_max = [3.5, 5.0]
-# Cg_min = [0.9, 2.0]
-# Cg_max = [3.5, 5.0]
-# w_min = [0.8, 2.0]
-# w_max = [2.0, 3.0]
-# gwn_std_dev = [0.2, 1.0]
+Cp_min = [0.1, 0.5]
+Cp_max = [2.5, 3.5]
+Cg_min = [0.5, 0.9]
+Cg_max = [2.5, 3.5]
+w_min = [0.3, 0.4]
+w_max = [0.9, 1.7]
+gwn_std_dev = [0.07, 0.15]
 
-# random_Cp_min = generate_uniform_numbers(Cp_min[0], Cp_min[1], count)
-# random_Cp_max = generate_uniform_numbers(Cp_max[0], Cp_max[1], count)
-# random_Cg_min = generate_uniform_numbers(Cg_min[0], Cg_min[1], count)
-# random_Cg_max = generate_uniform_numbers(Cg_max[0], Cg_max[1], count)
-# random_w_min = generate_uniform_numbers(w_min[0], w_min[1], count)
-# random_w_max = generate_uniform_numbers(w_max[0], w_max[1], count)
-# random_gwn_std_dev = generate_uniform_numbers(
-#     gwn_std_dev[0], gwn_std_dev[1], count)
+random_Cp_min = generate_uniform_numbers(Cp_min[0], Cp_min[1], count)
+random_Cp_max = generate_uniform_numbers(Cp_max[0], Cp_max[1], count)
+random_Cg_min = generate_uniform_numbers(Cg_min[0], Cg_min[1], count)
+random_Cg_max = generate_uniform_numbers(Cg_max[0], Cg_max[1], count)
+random_w_min = generate_uniform_numbers(w_min[0], w_min[1], count)
+random_w_max = generate_uniform_numbers(w_max[0], w_max[1], count)
+random_gwn_std_dev = generate_uniform_numbers(
+    gwn_std_dev[0], gwn_std_dev[1], count)
 
 
 # print(random_Cp_min)
@@ -56,16 +56,24 @@ count = 100
 # print(random_w_max)
 # print(random_gwn_std_dev)
 
-c1 = [1.49445, 2.0]
-c2 = [1.49445, 2.0]
-inertia = [0.729, 0.8]
+# c1 = [1.49445, 2.0]
+# c2 = [1.49445, 2.0]
+# inertia = [0.729, 0.8]
 
-random_c1 = generate_uniform_numbers(c1[0], c1[1], count)
-random_c2 = generate_uniform_numbers(c2[0], c2[1], count)
-random_inertia = generate_uniform_numbers(inertia[0], inertia[1], count)
+# random_c1 = generate_uniform_numbers(c1[0], c1[1], count)
+# random_c2 = generate_uniform_numbers(c2[0], c2[1], count)
+# random_inertia = generate_uniform_numbers(inertia[0], inertia[1], count)
 
-print(random_c1)
+print(random_Cp_min)
 print("--------------------------------")
-print(random_c2)
+print(random_Cp_max)
 print("--------------------------------")
-print(random_inertia)
+print(random_Cg_min)
+print("--------------------------------")
+print(random_Cg_max)
+print("--------------------------------")
+print(random_w_min)
+print("--------------------------------")
+print(random_w_max)
+print("--------------------------------")
+print(random_gwn_std_dev)
